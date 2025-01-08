0095388edc38041318ba0098d005d86356bfa289
grateqq <156215485+grateqq@users.noreply.github.com>
2025-01-08 16:37:10 -0300

    Creado tablero

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,30 @@
 import './App.css'
 
+const Turns = {
+  X: "x",
+  o: "o"
+}
+
+const board = Array(9).fill(null);
 function App() {
   
 
   return (
-    <h1>TIC TAC TOE</h1>
+ <main className='board'>
+  <h1>TIC TAC TOE</h1>
+  <section className='game'> {
+    board.map((_, index) => {return (
+      <div className='cell' key={index}>
+        <span className='cell_content'>
+          {index}
+        </span>
+      </div>)
+    } )
+    }
+
+  </section>
+ </main> 
+    
   )
 }
 
