631ca1173e1c11818b7b613d7e025f6f0d491635
grateqq <156215485+grateqq@users.noreply.github.com>
2025-01-13 00:03:16 -0300

    crado el componente square

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,4 @@
+import { Children } from 'react';
 import './App.css'
 
 const Turns = {
@@ -6,22 +7,29 @@ const Turns = {
 }
 
 const board = Array(9).fill(null);
+const Square = ({children, upadteBoard, index}) => {
+  return (
+    <div className='square'>
+      {children}
+    </div>
+  )
+}
+
 function App() {
   
+
 
   return (
  <main className='board'>
   <h1>TIC TAC TOE</h1>
   <section className='game'> {
     board.map((_, index) => {return (
-      <div className='cell' key={index}>
-        <span className='cell_content'>
-          {index}
-        </span>
-      </div>)
+      <Square key={index} index={index}>
+        {index+9} 
+      </Square>
+    )
     } )
     }
-
   </section>
  </main> 
     
